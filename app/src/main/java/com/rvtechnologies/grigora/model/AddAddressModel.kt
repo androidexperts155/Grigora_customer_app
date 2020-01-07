d9a18897fc7c71cf94b02bee38a8158ad5c9f8b4
user <webexpert4rv@gmail.com>
2020-01-07 14:53:30 +0530

    added addresses list

---
+++ app/src/main/java/com/rvtechnologies/grigora/model/AddAddressModel.kt
@@ -0,0 +1,29 @@
+package com.rvtechnologies.grigora.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class AddAddressModel(
+    @SerializedName("address")
+    var address: String = "",
+    @SerializedName("complete_address")
+    var completeAddress: String = "",
+    @SerializedName("complete_french_address")
+    var completeFrenchAddress: String = "",
+    @SerializedName("created_at")
+    var createdAt: String = "",
+    @SerializedName("french_address")
+    var frenchAddress: String = "",
+    @SerializedName("id")
+    var id: Int = 0,
+    @SerializedName("latitude")
+    var latitude: String = "",
+    @SerializedName("location_type_id")
+    var locationTypeId: String = "",
+    @SerializedName("longitude")
+    var longitude: String = "",
+    @SerializedName("updated_at")
+    var updatedAt: String = "",
+    @SerializedName("user_id")
+    var userId: Int = 0
+)
